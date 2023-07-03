710735a2bc00740278338e7afe331bb90aeb973d
Merdin <merdin10@icloud.com>
2023-07-03 18:18:20 +0200

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Merdin Kahrimanovic on 03/07/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
+
