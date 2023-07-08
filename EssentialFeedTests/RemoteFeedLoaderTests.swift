a0c6cdbee6fc8abe61ebe9d3039b5f0ceace8dcb
Merdin <merdin10@icloud.com>
2023-07-08 13:45:06 +0200

    Move `enumarated()` to other line

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -51,9 +51,9 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	func test_load_deliversErrorOnNon200HTTPResponse() {
 		let (sut, client) = makeSUT()
 		
-		let samples = [199, 201, 300, 400, 500].enumerated()
+		let samples = [199, 201, 300, 400, 500]
 		
-		samples.forEach { index, code in
+		samples.enumerated().forEach { index, code in
 			var capturedErrors = [RemoteFeedLoader.Error]()
 			sut.load { capturedErrors.append($0) }
 			
