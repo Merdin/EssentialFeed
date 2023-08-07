304957606f921b5040923a42b36fcec9321065fa
Merdin <merdin10@icloud.com>
2023-08-07 15:10:09 +0200

    Deleting an already empty cache has no side-effects (cache stays empty and does not fail)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -66,6 +66,11 @@ class CodableFeedStore {
 			completion(error)
 		}
 	}
+	
+	
+	func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+		completion(nil)
+	}
 }
 
 final class CodableFeedStoreTests: XCTestCase {
@@ -157,6 +162,17 @@ final class CodableFeedStoreTests: XCTestCase {
 		XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
 	}
 	
+	func test_delete_hasNoSideEffectsOnEmptyCache() {
+		let sut = makeSUT()
+		
+		let deletionError = deleteCache(from: sut)
+		
+		XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
+		expect(sut, toRetrieve: .empty)
+	}
+	
+
+	
 	// MARK: Helpers
 	
 	private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
@@ -206,6 +222,17 @@ final class CodableFeedStoreTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 		
 		return insertionError
+	}
+	
+	private func deleteCache(from sut: CodableFeedStore) -> Error? {
+		let exp = expectation(description: "Wait for cache deletion")
+		var deletionError: Error?
+		sut.deleteCachedFeed { receivedDeletionError in
+			deletionError = receivedDeletionError
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1.0)
+		return deletionError
 	}
 	
 	private func testSpecificStoreURL() -> URL {
