982bb3141b07b0295c25293ed66099c048047395
Merdin <merdin10@icloud.com>
2023-08-08 22:01:30 +0200

    Add `EssentialFeedCacheIntegrationTests` target to separate to potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Merdin Kahrimanovic on 08-08-2023.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+	
+}
