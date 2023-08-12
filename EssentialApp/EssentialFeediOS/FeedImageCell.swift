d6bcf2dc02d38746f44898f285a6f9b8811b524c
Merdin <merdin10@icloud.com>
2023-08-12 15:39:51 +0200

    Render loaded feed on successful load completion

---
+++ EssentialApp/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Merdin Kahrimanovic on 12-08-2023.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+	public let locationContainer = UIView()
+	public let locationLabel = UILabel()
+	public let descriptionLabel = UILabel()
+}
