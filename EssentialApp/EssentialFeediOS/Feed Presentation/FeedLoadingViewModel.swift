20b7ce30335d1ef5a57d03e26e17c4fdaf41a628
Merdin <merdin10@icloud.com>
2023-08-19 15:24:29 +0200

    Move ViewModels to separate files

---
+++ EssentialApp/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Merdin Kahrimanovic on 19-08-2023.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+	let isLoading: Bool
+}
+
---
+++ EssentialApp/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Merdin Kahrimanovic on 19-08-2023.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+	let feed: [FeedImage]
+}
