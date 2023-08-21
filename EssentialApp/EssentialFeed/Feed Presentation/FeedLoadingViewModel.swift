8e4383ef2673f583824d32e18c396b4c96741e86
Merdin <merdin10@icloud.com>
2023-08-21 17:21:04 +0200

    Move `FeedLoadingViewModel` to its own file

---
+++ EssentialApp/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Merdin Kahrimanovic on 21-08-2023.
+//
+
+public struct FeedLoadingViewModel {
+	public let isLoading: Bool
+}
