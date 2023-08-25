770f041b2d4737c9c5feee058a1b327085074b0f
Merdin <merdin10@icloud.com>
2023-08-25 19:03:53 +0200

    Add UI Test Target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,10 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Merdin Kahrimanovic on 25-08-2023.
+//
+
+import XCTest
+
+class EssentialAppUIAcceptanceTests: XCTestCase {}
