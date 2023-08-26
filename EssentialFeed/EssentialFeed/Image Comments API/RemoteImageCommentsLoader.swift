c07bc64c2bacaf10a204a9c222a0365a85e7f028
Merdin <merdin10@icloud.com>
2023-08-26 14:04:11 +0200

    Fix warnings

--- EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
@@ -29,7 +29,7 @@ public final class RemoteImageCommentsLoader {
 			guard self != nil else { return }
 
 			switch result {
-				case let .success(data, response):
+				case let .success((data, response)):
 					completion(RemoteImageCommentsLoader.map(data, from: response))
 				case .failure:
 					completion(.failure(Error.connectivity))
