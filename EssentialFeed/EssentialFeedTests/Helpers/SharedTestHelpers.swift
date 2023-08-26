539993a4b2d68171eb05fb9c1a412b3ca790a705
Merdin <merdin10@icloud.com>
2023-08-26 16:02:10 +0200

    Move test helpers to shared scope

--- EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -18,3 +18,14 @@ func anyData() -> Data {
 func anyNSError() -> NSError {
 	return NSError(domain: "any error", code: 0)
 }
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+	let json = ["items": items]
+	return try! JSONSerialization.data(withJSONObject: json)
+}
+
+extension HTTPURLResponse {
+	convenience init(statusCode: Int) {
+		self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+	}
+}
